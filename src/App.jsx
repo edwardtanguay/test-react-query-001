bb17d919ca23de34658ce2ec1c6ff4d2906defdd
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-17 18:58:03 +0200

    data.data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import './App.scss'
+import './App.scss';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const techBooksUrl = 'http://localhost:4222/techBooks';
 
 function App() {
-  return (
-    <div className="App">
-		<h1>Info Site</h1>
-		<p>Welcome to this site.</p>
-    </div>
-  )
+	const { data, error, isLoading } = useQuery(
+		'techBooks',
+		async () => await axios.get(techBooksUrl)
+	);
+
+	return (
+		<div className="App">
+			<h1>Info Site</h1>
+
+			{error && (
+				<div className="error">Error: {error.message}</div>
+			)}
+			{isLoading && (
+				<div className="isLoading">Loading...</div>
+			)}
+
+			{!error && !isLoading && data.data.length > 0 && (
+				<div className="content">
+					<pre>
+						{JSON.stringify(data, null, 2)}
+					</pre>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+	<QueryClientProvider client={queryClient}>
+		<App />
+	</QueryClientProvider>,
+	document.getElementById('root')
+);
